020665e4a0d39f8778d8afdf3a95707f26e6045f
Dan Bonistalli <danbonistalli@gmail.com>
2023-05-14 21:26:05 -0400

    Add files via upload

---
+++ python-demo-app-script/native_app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env -S python3 -u
+
+# Note that running python with the `-u` flag is required on Windows,
+# in order to ensure that stdin and stdout are opened in binary, rather
+# than text, mode.
+
+import sys
+import json
+import struct
+
+# Read a message from stdin and decode it.
+def getMessage():
+    rawLength = sys.stdin.buffer.read(4)
+    if len(rawLength) == 0:
+        sys.exit(0)
+    messageLength = struct.unpack('@I', rawLength)[0]
+    message = sys.stdin.buffer.read(messageLength).decode('utf-8')
+    return json.loads(message)
+
+# Read patient context stream, save to file
+while True:
+    receivedMessage = getMessage()
+    f = open("/Applications/djbhelper/context.ndjson", "a")
+    f.write("\r\n{0}".format(receivedMessage))
+    f.close()
