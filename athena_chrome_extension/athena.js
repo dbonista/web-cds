872c4cf53fd7789511bf552731f34f134efe775c
Daniel Bonistalli <ia0341@IA0341.local>
2023-05-16 10:33:26 -0400

    add refresh icon to Athena for manual refreshing

--- athena_chrome_extension/athena.js
+++ athena_chrome_extension/athena.js
@@ -3,6 +3,36 @@
 //set up connection port with app
 
 if (window.name == 'frMain') { //only load if we're in the clinical frame
+
+	function changeClass(elem){
+		console.log('click');
+		elem.classList.remove('icon-streamlined-spinner');
+		elem.classList.add('finite-scrolling-spinner');
+	}
+
+	const parentElems = document.getElementsByClassName("patient-demographic-items");
+	Array.prototype.forEach.call(parentElems, element => {
+		const listElem = document.createElement("li");
+		const iconContainer = document.createElement("div");
+		iconContainer.className = "chart-alert-container autostart";
+		iconContainer.title = "Click to refresh patient clinical content"
+		const iconDiv = document.createElement("div");
+		iconDiv.className = "chart-alert-trigger";
+		const refresh = document.createElement("span");
+		refresh.className = "icon-streamlined-spinner"; //pulled from athena .css
+		refresh.onclick = function() {
+			this.classList.remove('icon-streamlined-spinner');
+			this.classList.add('finite-scrolling-spinner');
+			this.style.bottom = "5px";
+			this.style.left = "10px";
+			window.location.reload();
+		};
+		iconDiv.appendChild(refresh);
+		iconContainer.appendChild(iconDiv);
+		listElem.appendChild(iconContainer);
+		element.appendChild(listElem);
+	})
+
 	var target = {'dt': new Date().toLocaleString()}; //init response json
 
 	chrome.runtime.onMessage.addListener(
